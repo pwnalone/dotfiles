c48523d981d7e0b7fdf6a432221dc7b9143a9d79
Joshua Inscoe <jinscoe123@gmail.com>
2024-02-12 20:47:11 -0800

    Set 'XDG_*' constants in scripts if not already set

--- scripts/install-nerd-font.sh
+++ scripts/install-nerd-font.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+
 FONTNAME="${1:-Hermit}"
 FONTPATH="$XDG_DATA_HOME/fonts/$FONTNAME"
 wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$FONTNAME.zip"
