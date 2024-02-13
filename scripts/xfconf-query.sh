bed97aad345c1f863800ae11e9e75214adbd3606
Joshua Inscoe <jinscoe123@gmail.com>
2024-02-12 22:39:00 -0800

    Configure DPMS and keyboard layouts in one place only

--- scripts/xfconf-query.sh
+++ scripts/xfconf-query.sh
@@ -15,24 +15,18 @@ xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-home        -s f
 xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-removable   -s false
 xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-trash       -s false
 
-# Edit keyboard layouts.
-xfconf-query -c keyboard-layout -p /Default/XkbLayout -n -t string -t string -s us -s ru
-xfconf-query -c keyboard-layout -p /Default/XkbOptions/Control -n -t string -s ctrl:nocaps
-xfconf-query -c keyboard-layout -p /Default/XkbOptions/Compose -n -t string -s compose:ralt
-xfconf-query -c keyboard-layout -p /Default/XkbOptions/Group -n -t string -s grp:ctrl_alt_toggle
-
 # Enable reverse scrolling for touchpads.
 for property in $(xfconf-query -c xfce4-desktop -l | grep 'Touchpad/ReverseScrolling'); do
     xfconf-query -c xfce4-desktop -p "$property" -s true
 done
 
 # Set timeouts to turn off the screen when on AC/battery power.
-xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-ac           15
-xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-ac-off        15
-xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-ac-sleep      15
-xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-battery      10
-xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-battery-off   10
-xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-battery-sleep 10
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-ac           -n -t int 15
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-ac-off        -n -t int 15
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-ac-sleep      -n -t int 15
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-battery      -n -t int 10
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-battery-off   -n -t int 10
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-battery-sleep -n -t int 10
 
 # Remap keyboard shortcuts.
 user_terminal_command=$(xfconf-query -c xfce4-keyboard-shortcuts -p '/commands/default/<Primary><Alt>t' -v)
