0037b07619b32658e024934ab6c23475ca663eef
Joshua Inscoe <jinscoe123@gmail.com>
2024-02-23 21:34:59 -0800

    Add zsh utility function to swap files, directories, etc.

--- .config/zsh/config/exports.zsh
+++ .config/zsh/config/exports.zsh
@@ -2,3 +2,10 @@
 [[ $PATH =~ "$HOME/.cargo/bin" ]] || export PATH="$HOME/.cargo/bin:$PATH"
 [[ $PATH =~ "$HOME/.r2env/bin" ]] || export PATH="$HOME/.r2env/bin:$PATH"
 [[ $PATH =~ "$GOPATH/bin" ]] || export PATH="$GOPATH/bin:$PATH"
+
+# Swap files, directories, links, etc.
+function swap
+{
+    local T=$(mktemp -d --tmpdir swap.XXXXXXXXXX) \
+        && mv "$1" "$T" && mv "$2" "$1" && mv "$T/$1" "$2" && rmdir "$T"
+}
