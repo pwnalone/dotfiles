cb6ba19b3f63304767f544943f519744ba210c1f
Joshua Inscoe <jinscoe123@gmail.com>
2025-11-27 22:01:01 +0200

    Update the installation script for Kali Linux

--- install.sh
+++ install.sh
@@ -4,75 +4,68 @@
 
 set -Eeuo pipefail
 
-#
+THREADS=${THREADS:-$(nproc)}
+
+NERD_FONTS_URI="https://github.com/ryanoasis/nerd-fonts"
+
+# Specify "all" or an array of Nerd Fonts to install.
+NERD_FONTS=(Hermit)
+
+# Update the package list and perform a full upgrade.
+sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
+
 # Install some utilities for a nicer dev environment.
-#
-# Use yay instead of pacman to avoid having to enter a password if everything is already installed.
-#
-yay --noconfirm --needed -S \
+sudo apt install -y \
     alacritty \
     bat \
     binutils \
-    bridge-utils \
+    cargo \
     checksec \
     clang \
     cmake \
     curl \
     diffutils \
-    distrobox \
-    dog \
     duf \
     fastfetch \
-    fd \
+    fd-find \
     findutils \
-    flatpak \
+    fonts-noto \
     fzf \
     gcc \
     gdb \
+    gdisk \
     git \
-    gptfdisk \
     htop \
     imagemagick \
-    inetutils \
     iproute2 \
-    iputils \
     iw \
     jq \
     lazygit \
-    ldns \
     less \
     lsd \
     ltrace \
     make \
     man-db \
-    man-pages \
+    manpages \
+    manpages-dev \
     meson \
     moreutils \
     ncdu \
     neovim \
-    nerd-fonts \
     net-tools \
-    node \
-    noto-fonts \
-    noto-fonts-cjk \
-    noto-fonts-emoji \
-    noto-fonts-extra \
+    nodejs \
     npm \
-    openbsd-netcat \
-    openssh \
-    pacman-contrib \
+    openssh-client \
+    openssh-server \
     pciutils \
     plocate \
     podman \
-    pwndbg \
     ripgrep \
     rizin \
     ropper \
     socat \
-    starship \
     strace \
     tcpdump \
-    tectonic \
     tmux \
     tokei \
     tree \
@@ -102,3 +95,27 @@ done
 # Download Tmux plugins with the Tmux Plugin Manager.
 git clone https://github.com/tmux-plugins/tpm $XDG_CONFIG_HOME/tmux/plugins/tpm
 $XDG_CONFIG_HOME/tmux/plugins/tpm/bin/install_plugins
+
+# Install the specified Nerd Fonts or all Nerd Fonts.
+pushd $(mktemp -d)
+if [ "$NERD_FONTS" = "all" ]; then
+    #
+    # WARN: This may fail when run from a tmpfs.
+    #
+    # If this occurs, set TMPDIR to a non-tmpfs directory and try again.
+    #
+    git clone --depth=1 $NERD_FONTS_URI . && ./install.sh -q
+else
+    C="curl -fsSL $NERD_FONTS_URI/releases/latest/download/{}.tar.xz | tar -Jx"
+    parallel -j $THREADS -i sh -c "$C" -- "${NERD_FONTS[@]}"
+    install -Dt $XDG_DATA_HOME/fonts *.[ot]tf && fc-cache -f
+fi
+popd
+
+# Install programs that are not available with `apt`.
+pushd $(mktemp -d)
+cargo install --locked starship                                                          # Starship
+curl --proto '=https' --tlsv1.2 -fsSL https://drop-sh.fullyjustified.net | sh -s         # Tectonic
+curl --proto '=https' --tlsv1.2 -fsSL https://install.pwndbg.re | sh -s -- -t pwndbg-gdb # Pwndbg
+sudo install -Dm 0755 -t /usr/local/bin tectonic
+popd
